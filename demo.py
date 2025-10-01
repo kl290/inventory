24200e08975d68e2cafee18098525cf3b9c10175
kl <kl@intentus.de>
2025-10-01 10:18:18 +0200

    kleiner Nachtrag
    - Anpassung von demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import inventory
 inventory.add_item("Apfel", 1)
 inventory.add_item("Bananen", 10)
 inventory.add_item("Weintrauben", 10)
-inventory.add_item("Birnen", 4)
+inventory.add_item("Birnen", 5)
 
 print("Bestand Apfel:", inventory.get_stock("Apfel"))
 print("Bestand Bananen:", inventory.get_stock("Bananen"))
@@ -15,7 +15,10 @@ inventory.sell_item("Apfel", 1)
 inventory.sell_item("Bananen", 5)
 inventory.sell_item("Birnen", 4)
 
-print("Bestand Apfel nach Verkauf:", inventory.get_stock("Apfel"))
+try:
+    print("Bestand Apfel nach Verkauf:", inventory.get_stock("Apfel"))
+except ValueError:
+    print("Apfel ist nicht mehr im Lager.")
 print("Bestand Bananen nach Verkauf:", inventory.get_stock("Bananen"))
 print("Bestand Weintrauben nach Verkauf:", inventory.get_stock("Weintrauben"))
 print("Bestand Birnen nach Verkauf:", inventory.get_stock("Birnen"))
